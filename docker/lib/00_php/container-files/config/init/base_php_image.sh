cb01f6e09c8de711f70ca406ee352dad4db5a918
pietrobaricco <pietro.baricco@gmail.com>
2018-03-05 11:05:52 +0100

    fix

--- docker/lib/00_php/container-files/config/init/base_php_image.sh
+++ docker/lib/00_php/container-files/config/init/base_php_image.sh
@@ -11,3 +11,14 @@ else
     echo "disabling debugger..."
     /usr/bin/php_debugger_disable.sh
 fi
+
+
+# init shared folder here, so that no volume is created at build time and
+# /shared can hence work with host mappings in compose.yml for derived images
+
+mkdir /shared/xdebug_output || true
+mkdir /shared/tmp || true
+mkdir /shared/log || true
+chmod 777 /shared/xdebug_output
+chmod 777 /shared/tmp
+chmod 777 /shared/log
